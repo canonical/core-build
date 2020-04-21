d7000638cc1a9c51436183baa04c37961922ee0a
Michael Vogt <mvo@ubuntu.com>
2020-04-21 18:28:08 +0200

    initramfs: add tests/fix code

---
+++ initramfs/tests/test_ubuntu-core-rootfs.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -e
+
+scriptsroot=./scripts
+# shellcheck source=./scripts/ubuntu-core-rootfs
+. scripts/ubuntu-core-rootfs
+
+
+rootmnt="$(mktemp -d)"
+trap 'rm -rf "$rootmnt"' EXIT
+
+# test: no _writable_defaults does not break
+mkdir -p "$rootmnt/writeable/system-data/"
+handle_writable_defaults
+
+# test: empty _writable_defaults does not break
+mkdir -p "$rootmnt/writable/system-data/_writable_defaults"
+handle_writable_defaults
+test -e "$rootmnt/writable/system-data/_writable_defaults/.done"
+# cleanup
+rm -f "$rootmnt/writable/system-data/_writable_defaults/.done"
+
+# test: file/dir in _writable_defaults
+mkdir -p "$rootmnt/writable/system-data/_writable_defaults/some-dir"
+touch "$rootmnt/writable/system-data/_writable_defaults/some-dir/some-file"
+touch "$rootmnt/writable/system-data/_writable_defaults/other-file"
+
+handle_writable_defaults
+# ensure we have the .done file
+test -e "$rootmnt/writable/system-data/_writable_defaults/.done"
+test -d "$rootmnt/writable/system-data/some-dir"
+test -e "$rootmnt/writable/system-data/some-dir/some-file"
+test -e "$rootmnt/writable/system-data/other-file"
+# cleanup
+rm -f "$rootmnt/writable/system-data/_writable_defaults/.done"
